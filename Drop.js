ac259d5a6c6dd80ab169649a210479ec9ed74a3b
abzdev <abzdvlpmnt@gmail.com>
2021-02-23 17:47:25 -0800

    added drop.js

---
+++ Drop.js
@@ -0,0 +1,20 @@
+class Drop {
+    constructor(x,y,r) {
+        var options = {
+            restitution: 0.8,
+            friction: 0.5
+        }
+        this.body = Bodies.circle(x,y,r,options);
+        World.add(world,this.body);
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    display() {
+        fill('blue');
+        circle(this.x,this.y,this.r*2);
+        if(frameCount%60 === 0) {
+            console.log(this.body.position.y);
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,24 +1,35 @@
 const World = Matter.World;
-const Engine = Matter.engine;
+const Engine = Matter.Engine;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var engine, world;
 
-var manAnimation, umbrella, thunder1, thunder2, thunder3, thunder4;
+var walkingImage, umbrella, thunder1, thunder2, thunder3, thunder4;
+var drops = []
 
-function preload(){
-    manAnimation = loadAnimation('walking_1.png','walking_2.png','walking_3.png','walking_4.png','walking_5.png','walking_6.png','walking_7.png','walking_8.png');
+function preload() {
+    walkingImage= loadImage('images/Walking\ Frame/walking_1.png');
 }
 
-function setup(){
+function setup() {
     createCanvas(400,800);
     engine = Engine.create();
     world = engine.world;
+
+    for (var i = 0; i < 100; i++) {
+        drops.push(new Drop(random(0,400),10,3));
+    }
 }
 
-function draw(){
+function draw() {
     imageMode(CENTER);
     ellipseMode(CENTER);
-    image(manAnimation,width/2,700);
+    background(0);
+    walkingImage.resize(400,400);
+    image(walkingImage,width/2-30,600);
+
+    for(var i = 0; i < 100; i++) {
+        drops[i].display();
+    }
 }
 
